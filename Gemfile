0bc108e0b70c96b223dbbab9267273a45c4b06db
Tea <50013396+RITEAHOO@users.noreply.github.com>
2022-07-31 23:11:12 +0900

    name

--- Gemfile
+++ Gemfile
@@ -1,2 +1,16 @@
-source "https://rubygems.org"
-gemspec
+source 'https://rubygems.org'
+gem 'fastlane'
+
+gem "jekyll", "~> 3.9"
+
+gem "jekyll-paginate", "~> 1.1"
+
+gem "jekyll-sitemap", "~> 1.4"
+
+gem "jekyll-gist", "~> 1.5"
+
+gem "jekyll-feed", "~> 0.16.0"
+
+gem "jekyll-include-cache", "~> 0.2.1"
+
+gem "kramdown-parser-gfm", "~> 1.1"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Layout: Post with Sticky Table of Contents",
+        "excerpt":"“Stick” table of contents to the top of a page by adding toc_sticky: true to its YAML Front Matter. --- toc: true toc_sticky: true --- HTML Elements Below is just about everything you’ll need to style in the theme. Check the source code to see the many embedded elements within...","categories": [],
+        "tags": ["table of contents"],
+        "url": "/layout-table-of-contents-sticky/",
+        "teaser": null
+      },{
+        "title": "Markup: HTML Tags and Formatting",
+        "excerpt":"A variety of common markup showing how the theme styles them. Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on....","categories": ["Markup"],
+        "tags": ["content","css","formatting","html","markup"],
+        "url": "/markup/markup-html-tags-and-formatting/",
+        "teaser": "/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Layout: Header Video",
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=-PVofD2A9t8 (long version) or https://youtu.be/-PVofD2A9t8...","categories": ["Layout","Uncategorized"],
+        "tags": ["video","layout"],
+        "url": "/layout/uncategorized/layout-header-video/",
+        "teaser": null
+      },{
+        "title": "하이",
+        "excerpt":"“Stick” table of contents to the top of a page by adding toc_sticky: true to its YAML Front Matter. --- toc: true toc_sticky: true --- HTML Elements Below is just about everything you’ll need to style in the theme. Check the source code to see the many embedded elements within...","categories": [],
+        "tags": ["table of contents"],
+        "url": "/project-review/",
+        "teaser": null
+      }]
